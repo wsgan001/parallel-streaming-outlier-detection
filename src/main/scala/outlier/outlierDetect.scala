549b37b3950d3f0d9f7bd24fa160f3f715c753f5
Theo Toliopoulos <th.toliopoulos@gmail.com>
2018-02-05 10:37:22 +0200

    baseline

--- src/main/scala/outlier/outlierDetect.scala
+++ src/main/scala/outlier/outlierDetect.scala
@@ -21,10 +21,10 @@ object outlierDetect {
   //count window variables (total / partitions)
   var count_window: Int = 10000
   var count_slide: Int = 500
-  val count_slide_percent: Double = 100 * (count_slide.toDouble / count_window)
+  var count_slide_percent: Double = 100 * (count_slide.toDouble / count_window)
   //time window variables
-  val time_window: Int = count_window / 10
-  val time_slide: Int = (time_window * (count_slide_percent / 100)).toInt
+  var time_window: Int = count_window / 10
+  var time_slide: Int = (time_window * (count_slide_percent / 100)).toInt
   //distance outlier variables
   val k: Int = 50
   val range: Double = 0.45
@@ -47,6 +47,10 @@ object outlierDetect {
     count_window = args(1).toInt
     count_slide = args(2).toInt
     data_input = args(3)
+    count_slide_percent = 100 * (count_slide.toDouble / count_window)
+    //time window variables
+    time_window = count_window / 10
+    time_slide = (time_window * (count_slide_percent / 100)).toInt
 
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(parallelism)
