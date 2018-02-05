cf11593f02a582e4ea071e1cddbfabd81528c401
Theo Toliopoulos <th.toliopoulos@gmail.com>
2018-02-05 15:17:50 +0200

    mtree

--- src/main/scala/outlier/outlierDetect.scala
+++ src/main/scala/outlier/outlierDetect.scala
@@ -51,9 +51,10 @@ object outlierDetect {
   //helper to slow down stream
   val cur_time = System.currentTimeMillis() + 1000000L //some delay for the correct timestamp
   //hardcoded spatial partitioning
-  val spatial = Map[Int, String](4 -> "97.633!100.37!104.25",
+  val spatial = Map[Int, String](2 -> "100.37",
+    4 -> "97.633!100.37!104.25",
     8 -> "94.222!97.633!99.25!100.37!102.13!104.25!106.65",
-    12->"90.7!95.965!97.633!98.75!99.7!100.37!101.49!102.84!104.25!105.59!108.36",
+    12 -> "90.7!95.965!97.633!98.75!99.7!100.37!101.49!102.84!104.25!105.59!108.36",
     32 -> "77.457!87.231!91.88!94.222!95.59!96.5!97.125!97.633!98.074!98.5!98.888!99.25!99.588!99.897!100.07!100.37!100.72!101.16!101.65!102.13!102.65!103.18!103.72!104.25!104.78!105.25!105.79!106.65!107.84!109.75!112.14",
     16 -> "87.231!94.222!96.5!97.633!98.5!99.25!99.897!100.37!101.16!102.13!103.18!104.25!105.25!106.65!109.75")
 
@@ -64,8 +65,8 @@ object outlierDetect {
     if (args.length != 4) {
       println("Wrong arguments!")
       System.exit(1)
-    } else if (args(0).toInt != 8 && args(0).toInt != 4 && args(0).toInt != 12 && args(0).toInt != 32 && args(0).toInt != 16) {
-      println("Parallelism should be 4, 8, 12, 32 or 16!")
+    } else if (args(0).toInt != 2 && args(0).toInt != 8 && args(0).toInt != 4 && args(0).toInt != 12 && args(0).toInt != 32 && args(0).toInt != 16) {
+      println("Parallelism should be 2, 4, 8, 12, 32 or 16!")
       System.exit(1)
     }
 
@@ -80,16 +81,17 @@ object outlierDetect {
 
     var points_string = List[String]()
     if (parallelism == 8) {
-      //points [0-6]
       points_string = spatial(8).split("!").toList
     } else if (parallelism == 32) {
       points_string = spatial(32).split("!").toList
     } else if (parallelism == 16) {
       points_string = spatial(16).split("!").toList
     } else if (parallelism == 12) {
-    points_string = spatial(12).split("!").toList
+      points_string = spatial(12).split("!").toList
     } else if (parallelism == 4) {
       points_string = spatial(4).split("!").toList
+    } else if (parallelism == 2) {
+      points_string = spatial(2).split("!").toList
     }
     val points = points_string.map(_.toDouble)
 
@@ -110,39 +112,40 @@ object outlierDetect {
         var i = 0
         var break = false
         var belongs_to, previous, next = -1
-        do {
-          if (value <= points(i)) {
-            belongs_to = i //belongs to the current partition
-            break = true
-            if (i != 0) {
-              //check if it is near the previous partition
-              if (value <= points(i - 1) + range) {
-                previous = i - 1
+          do {
+            if (value <= points(i)) {
+              belongs_to = i //belongs to the current partition
+              break = true
+              if (i != 0) {
+                //check if it is near the previous partition
+                if (value <= points(i - 1) + range) {
+                  previous = i - 1
+                }
+              } //check if it is near the next partition
+              if (value >= points(i) - range) {
+                next = i + 1
               }
-            } //check if it is near the next partition
-            if (value >= points(i) - range) {
-              next = i + 1
+            }
+            i += 1
+          } while (i <= parallelism - 2 && !break)
+          if (!break) {
+            // it belongs to the last partition
+            belongs_to = parallelism - 1
+            if (value <= points(parallelism - 2) + range) {
+              previous = parallelism - 2
             }
           }
-          i += 1
-        } while (i <= parallelism - 2 && !break)
-        if (!break) {
-          // it belongs to the last partition
-          belongs_to = parallelism - 1
-          if (value <= points(parallelism - 2) + range) {
-            previous = parallelism - 2
+          val tmpEl = (belongs_to, new Data1d(value, new_time, 0, id))
+          list.+=(tmpEl)
+          if (previous != -1) {
+            val tmpEl2 = (previous, new Data1d(value, new_time, 1, id))
+            list.+=(tmpEl2)
           }
-        }
-        val tmpEl = (belongs_to, new Data1d(value, new_time, 0, id))
-        list.+=(tmpEl)
-        if (previous != -1) {
-          val tmpEl2 = (previous, new Data1d(value, new_time, 1, id))
-          list.+=(tmpEl2)
-        }
-        if (next != -1) {
-          val tmpEl2 = (next, new Data1d(value, new_time, 1, id))
-          list.+=(tmpEl2)
-        }
+          if (next != -1) {
+            val tmpEl2 = (next, new Data1d(value, new_time, 1, id))
+            list.+=(tmpEl2)
+          }
+
         list
       })
 
@@ -225,7 +228,7 @@ object outlierDetect {
           }
         }
         val mySplit = new ComposedSplitFunction[Data1d](nonRandomPromotion, new PartitionFunctions.BalancedPartition[Data1d])
-        val myTree = new MTree[Data1d](k, 2* count_window, DistanceFunctions.EUCLIDEAN, mySplit)
+        val myTree = new MTree[Data1d](k, 2 * count_window, DistanceFunctions.EUCLIDEAN, mySplit)
         for (el <- elements) {
           myTree.add(el._2)
         }
@@ -249,9 +252,9 @@ object outlierDetect {
           while (iter.hasNext) {
             val node = iter.next().data
             if (node.id != tmpData.id) {
-              if(node.arrival >= tmpData.arrival){
+              if (node.arrival >= tmpData.arrival) {
                 tmpData.count_after += 1
-              }else{
+              } else {
                 tmpData.insert_nn_before(node.arrival, k)
               }
             }
